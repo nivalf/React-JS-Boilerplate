0cafbfebf53241a04c6969215c759d5510d7e828
Flavin John <Flavinj@WinjitTechnologiesPvtLtd.local>
2021-07-02 14:37:43 +0530

    Updates

--- src/pages/Public/About/index.js
+++ src/pages/Public/About/index.js
@@ -1,7 +1,4 @@
 import React from "react";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import { uiConfig } from "../../../settings";
-import { firebaseAuth } from "../../../utility";
 import { Card } from "../../../components";
 
 const About = () => <Card title="About"></Card>;
